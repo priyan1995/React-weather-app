2349fc55068a70abe8b4c46bced97ff0164da15f
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-09 16:14:03 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import './App.css';
 import { Weather } from './components/Weather';
 
 function App() {
   return (
-    <div className="App">
-      <Weather />
-    </div>
+    <>
+      <div className="App">
+        <Router>
+
+          <div className="nav-card">
+            <Link to="/colombo-weather">Colombo Weather</Link>
+          </div>
+
+
+          <Switch>
+            <Route path="/colombo-weather">
+              <Weather />
+            </Route>
+
+          </Switch>
+
+
+        </Router>
+      </div>
+    </>
   );
 }
 
