7a31f4146fd6e89689e210cc8721326a99fa9f7d
priyan1995 <priyan.abewickrama@ogilvy.com>
2021-11-11 11:44:48 +0530

    loader added for weather sheet

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -10,6 +10,8 @@ export const Weather = () => {
     const [mainWeatherData, setMainWeatherData] = useState([0]);
     const [windData, setWindData] = useState([])
 
+    const [loading, setLoading] = useState(true);
+
 
 
 
@@ -21,6 +23,7 @@ export const Weather = () => {
                 setWeatherInfColombo(response.data.weather)
                 setMainWeatherData(response.data.main)
                 setWindData(response.data.wind)
+                setLoading(false)
             })
     }, [curruntWeatherUrl])
 
@@ -35,59 +38,71 @@ export const Weather = () => {
     return (
         <>
 
-            <section className="weather-cards-sec">
-                <div className="container">
-                    <div className="row text-center">
-                        <div className="col-lg-12 main-title-sec">
-                            <h4>Weather Information -  {weatherColombo.name} </h4>
-                        </div>
-                    </div>
+            {loading ? (
+                <>
 
-                    <div className="row">
+                    <h2 className="text-center">Loading...</h2>
 
-                        <div className="col-lg-4">
-                            <div className="weather-card">
-                                <div>
-                                    <img src={` http://openweathermap.org/img/wn/${weatherInfColombo[0].icon}.png `} />
-                                    <h3>{weatherInfColombo[0].main}</h3>
-                                    <p>{weatherInfColombo[0].description}</p>
+
+                </>
+            ) : (
+                <>
+                    <section className="weather-cards-sec">
+                        <div className="container">
+                            <div className="row text-center">
+                                <div className="col-lg-12 main-title-sec">
+                                    <h4>Weather Information -  {weatherColombo.name} </h4>
                                 </div>
                             </div>
-                        </div>
 
-                        <div className="col-lg-4">
-                            <div className="main-weather-card">
-                                <div>
-                                    <h3>{mainWeatherData.temp}</h3>
-                                    <p>Temperature</p>
+                            <div className="row">
+
+                                <div className="col-lg-4">
+                                    <div className="weather-card">
+                                        <div>
+                                            <img src={` http://openweathermap.org/img/wn/${weatherInfColombo[0].icon}.png `} />
+                                            <h3>{weatherInfColombo[0].main}</h3>
+                                            <p>{weatherInfColombo[0].description}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div>
-                                    <h3>{mainWeatherData.feels_like}</h3>
-                                    <p>Feels Like</p>
+
+                                <div className="col-lg-4">
+                                    <div className="main-weather-card">
+                                        <div>
+                                            <h3>{mainWeatherData.temp}</h3>
+                                            <p>Temperature</p>
+                                        </div>
+                                        <div>
+                                            <h3>{mainWeatherData.feels_like}</h3>
+                                            <p>Feels Like</p>
+                                        </div>
+                                    </div>
                                 </div>
+
+
+                                <div className="col-lg-4">
+                                    <div className="wind-weather-card">
+                                        <div>
+                                            <h3>Wind</h3>
+                                            <p>Deg: <strong> {windData.deg}</strong></p>
+                                            <p>Speed: <strong>{windData.speed}</strong></p>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
                             </div>
+
+
+
+
                         </div>
+                    </section>
+                </>
+            )}
 
-
-                        <div className="col-lg-4">
-                            <div className="wind-weather-card">
-                                <div>
-                                    <h3>Wind</h3>
-                                    <p>Deg: <strong> {windData.deg}</strong></p>
-                                    <p>Speed: <strong>{windData.speed}</strong></p>
-                                </div>
-                            </div>
-                        </div>
-
-
-
-                    </div>
-
-
-
-
-                </div>
-            </section>
 
         </>
     )
